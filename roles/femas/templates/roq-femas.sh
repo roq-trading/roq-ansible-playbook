7d5c6bfadee6ac80d26cc6808074ea4946e82c6f
thraneh <thraneh@gmail.com>
2018-09-14 17:48:34 +0200

    making the services work

--- roles/femas/templates/roq-femas.sh
+++ roles/femas/templates/roq-femas.sh
@@ -2,34 +2,28 @@
 
 set -e
 
-CONDA_DIR="{{ root }}/miniconda"
-LOG_DIR="{{ root }}/var/log"
-CONFIG_DIR="{{ root }}/etc/roq/femas"
+# activate the conda environment
+source "{{ root }}/miniconda/bin/activate"
 
-LICENSE="$CONFIG_DIR/license.conf"
-VARIABLES="$CONFIG_DIR/variables.conf"
-CONFIG="$CONFIG_DIR/master.conf"
-
-SOCKET="{{ root }}/var/tmp/femas.sock"
-
-source "$CONDA_DIR/bin/activate"
-
+# preload tcmalloc, if present
 if [ -e "$CONDA_PREFIX/lib/libtcmalloc.so" ]; then
   export LD_PRELOAD="$CONDA_PREFIX/lib/libtcmalloc.so"
 fi
 
+# define logging parameters
 {% if femas.use_log_dir %}
-export ROQ_log_dir="$LOG_DIR"
+export ROQ_log_dir="{{ root }}/var/log/roq"
 {% endif %}
 export ROQ_v="{{ femas.verbosity | default(0) }}"
 
-"$CONDA_PREFIX/bin/roq-femasapi" \
+# launch the application
+"$CONDA_PREFIX/bin/roq-femas" \
     --socket-buffer-size 10485760 \
     --spin-usecs 1000 \
     --timer-refresh-usecs 100 \
-    --license-file "$LICENSE" \
-    --config-variables "$VARIABLES" \
-    --config-file "$CONFIG" \
-    --local-address "$SOCKET" \
+    --license-file "{{ root }}/etc/roq/femas/license.conf" \
+    --config-variables "{{ root }}/etc/roq/femas/variables.conf" \
+    --config-file "{{ root }}/etc/roq/femas/master.conf" \
+    --local-address "{{ root }}/var/tmp/femas.sock" \
     --monitor-port "12345" \
     --name "roq_femas"
---
+++ roles/samples/templates/roq-collector.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+# activate the conda environment
+source "{{ root }}/miniconda/bin/activate"
+
+# preload tcmalloc, if present
+if [ -e "$CONDA_PREFIX/lib/libtcmalloc.so" ]; then
+  export LD_PRELOAD="$CONDA_PREFIX/lib/libtcmalloc.so"
+fi
+
+# define logging parameters
+{% if collector.use_log_dir %}
+export ROQ_log_dir="{{ root }}/var/log/roq"
+{% endif %}
+export ROQ_v="{{ collector.verbosity | default(0) }}"
+
+# launch the application
+"$CONDA_PREFIX/bin/roq-samples-collector" \
+    --gateways "femas=test:1234@{{ root }}/var/tmp/femas.sock" \
+    >> "{{ root }}/var/lib/roq/femas_$(date +%Y-%m-%d).csv"
