648980dcf5e5e5f0812c70230a37134a294754bf
thraneh <thraneh@gmail.com>
2018-10-29 10:22:44 +0100

    using the --output-file option (allows us to start using docker)

--- roles/roq-samples/templates/roq-collector.sh
+++ roles/roq-samples/templates/roq-collector.sh
@@ -3,7 +3,7 @@
 set -e
 
 # activate the conda environment
-source "{{ root }}/miniconda/bin/activate"
+source "{{ root }}/miniconda/bin/activate" ""
 
 # preload tcmalloc, if present
 if [ -e "$CONDA_PREFIX/lib/libtcmalloc.so" ]; then
@@ -18,5 +18,4 @@ export ROQ_v="{{ collector_config.verbosity | default(0) }}"
 
 # launch the application
 "$CONDA_PREFIX/bin/roq-samples-collector" \
-    --gateways "femas=test:1234@{{ root }}/var/tmp/femas.sock" \
-    >> "{{ root }}/var/lib/roq/femas_$(date +%Y-%m-%d).csv"
+    --gateways "femas=test:1234@{{ root }}/var/tmp/femas.sock" $@
