e1119d578ac3f6c325e788e0d5bf6a20c5c96ce0
thraneh <thraneh@gmail.com>
2018-12-08 09:59:41 +0100

    ROQ-31 Backup scripts must wait for directory availability.

--- roles/postgres/templates/postgres-backup.sh
+++ roles/postgres/templates/postgres-backup.sh
@@ -2,10 +2,17 @@
 
 set -e
 
-if [ ! -d "{{ backups }}" ]; then
-  echo "{{ backups }} not ready!"
-  exit 1
-fi
+for i in {1..10}; do
+  if [ -d "{{ backups }}" ]; then
+    echo "{{ backups }} is ready!"
+    break
+  fi
+  if [ "$i" -eq "10" ]; then
+    echo "{{ backups }} is *not* ready!"
+    exit 1
+  fi
+  sleep 2
+done
 
 mkdir -p {{ backups }}/postgres/{raw,native}
 
