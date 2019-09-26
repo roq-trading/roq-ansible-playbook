579a59d219f53643f49b6ec95a54d65cafd86b90
thraneh <thraneh@gmail.com>
2019-09-26 09:13:20 +0200

    support thread pinning

--- roles/roq-influxdb/templates/roq-influxdb.sh
+++ roles/roq-influxdb/templates/roq-influxdb.sh
@@ -5,4 +5,12 @@ set -e
 ARGS="${@:-"--flagfile={{ root }}/etc/roq/roq-influxdb.gflags"}"
 
 "{{ root }}/miniconda/bin/roq-influxdb" "$ARGS" \
-  "{{ root }}/var/tmp/roq-simulator.roq"
+{% if 'roq_simulator' in groups %}
+  "{{ root }}/var/tmp/roq-simulator.roq" \
+{% endif %}
+{% if 'roq_deribit' in groups %}
+  "{{ root }}/var/tmp/roq-deribit.roq" \
+{% endif %}
+{% if 'roq_coinbase_pro' in groups %}
+  "{{ root }}/var/tmp/roq-coinbase-pro.roq"
+{% endif %}
