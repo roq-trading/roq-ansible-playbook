bd1823b72302440e131e0a7c2080c8505c7cf250
thraneh <thraneh@gmail.com>
2020-02-07 18:04:01 +0100

    event logs now written to {event-log-dir}/{iso-week}/{service-name}/{ts}.roq + centralise backup service

--- roles/roq-backup/templates/roq-backup.sh
+++ roles/roq-backup/templates/roq-backup.sh
@@ -14,9 +14,9 @@ for i in {1..10}; do
   sleep 2
 done
 
-mkdir -p "{{ backups }}/roq-gemini"
+mkdir -p "{{ backups }}"
 
-rsync -av "{{ root }}/var/lib/roq/roq-gemini/" "{{ backups }}/roq-gemini"
+rsync -av "{{ root }}/var/lib/roq/" "{{ backups }}"
 
 # TODO(thraneh): remove older files from source
 
